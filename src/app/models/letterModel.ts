d3a877fd9ac4de27b0f3d505456ca0ee7ab9d214
Daniel Robles <drm.electronic@gmail.com>
2021-09-26 01:28:18 -0500

    tildes, comas, espacios, palabras

--- src/app/models/letterModel.ts
+++ src/app/models/letterModel.ts
@@ -1,20 +1,45 @@
 export class LetterModel {
-    hidden: boolean;
+    class: string;
     letra: string;
     show: string;
 
     constructor(letra: string) {
         this.letra = letra;
-        this.show = '__';
-        this.hidden = this.letra === ' ';
-        console.log('letra', this.letra, 'hidden', this.hidden)
+        if (this.letra === ',' || this.letra === '.' || this.letra === ';') {
+            this.show = this.letra;
+            this.class = 'puntuacion';
+        } else {
+            this.show = '__';
+        }
+        if (this.letra === ' ') {
+            this.class = 'espacio';
+        }
+        // console.log('letra', this.letra, 'hidden', this.hidden)
     }
     
     verify(key: string): boolean {
         if (this.letra === key) {
             this.show = this.letra;
             return true;
-        };
+        } else if (this.letra === 'Á' && key === 'A') {
+            this.show = this.letra;
+            return true;
+        } else if (this.letra === 'É' && key === 'E') {
+            this.show = this.letra;
+            return true;
+        } else if (this.letra === 'Í' && key === 'I') {
+            this.show = this.letra;
+            return true;
+        } else if (this.letra === 'Ó' && key === 'O') {
+            this.show = this.letra;
+            return true;
+        } else if (this.letra === 'Ú' && key === 'U') {
+            this.show = this.letra;
+            return true;
+        } else if (this.letra === 'Ü' && key === 'U') {
+            this.show = this.letra;
+            return true;
+        }
         return false;
     }
 }
