2abfe040cacf805333ec1211243a89cb660ca93b
Daniel Robles <drm.electronic@gmail.com>
2021-12-27 23:24:40 -0500

    style flex

--- src/app/hangman/hangman.component.ts
+++ src/app/hangman/hangman.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { KeyInterface } from '../models/keyInterface';
-import { KeyModel } from '../models/keyModel';
-import { LetterModel } from '../models/letterModel';
+import {Component, OnInit} from '@angular/core';
+import {FRASES} from '../frases/pamela';
+import {KeyModel} from '../models/keyModel';
+import {LetterModel} from '../models/letterModel';
 
 @Component({
   selector: 'app-hangman',
@@ -9,32 +9,20 @@ import { LetterModel } from '../models/letterModel';
   styleUrls: ['./hangman.component.scss']
 })
 export class HangmanComponent implements OnInit {
-  
+
   level = 0;
-  frases: string[] = [
-      'NO QUIERO ANIQUILAR A NADIE, ODIO A LOS ABUSIVOS, SEAN DE DONDE SEAN.',
-      'ANTES DE EMPEZAR, ¿ALGUIEN PREFIERE RETIRARSE?',
-      'CUANDO NO TENÍA NADA, TENÍA A BUCKY.',
-      'EL PRECIO DE LA LIBERTAD ES ALTO, SIEMPRE LO HA SIDO Y ESTOY DISPUESTO A PAGAR EL PRECIO, SI DEBO PELEAR SÓLO QUE ASÍ SEA.',
-      'ÉSTA ES LA PELEA DE NUESTRAS VIDAS, Y VAMOS A GANAR, HAREMOS LO QUE SEA.',
-      'HARÍA ESTO TODO EL DÍA.',
-      'HACE CINCO AÑOS PERDIMOS TODOS, PERDIMOS AMIGOS, PERDIMOS FAMILIA Y UNA PARTE DE NOSOTROS.',
-      'SÓLO HAY UN DIOS PARA MÍ, Y JAMÁS SE VESTIRÍA ASÍ.',
-      'A TU IZQUIERDA.',
-      'YO SÍ, ES QUE SÍ ENTENDI LA REFERENCIA',
-      'YO SÉ QUE SÍ, PORQUE NO SÉ LO QUÉ VOY A HACER SI NO'
-  ];
+  frases: string[] = FRASES;
   frase: string;
   isMinLevel: boolean;
   isMaxLevel: boolean;
   letters: LetterModel[] = [];
   keys: KeyModel[] = [];
-  words: LetterModel[][]
+  words: LetterModel[][];
   fallas = 0;
 
   constructor() { }
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.setLevel();
   }
 
@@ -46,41 +34,43 @@ export class HangmanComponent implements OnInit {
     });
   }
 
-  keyPressed(event: string) {
-    let existe = false
+  keyPressed(event: string): void {
+    let existe = false;
     this.letters.forEach(l => {
       if (l.verify(event)) {
         existe = true;
-      }      
+      }
     });
     if (!existe) {
       this.fallas += 1;
     }
   }
 
-  anteriorNivel() {
+  anteriorNivel(): void {
     this.level -= 1;
-    this.setLevel()
-  }
-  
-  siguienteNivel() {
-    this.level += 1;
-    this.setLevel()
+    this.fallas = 0;
+    this.setLevel();
   }
 
-  setLevel() {
+  siguienteNivel(): void {
+    this.level += 1;
+    this.fallas = 0;
+    this.setLevel();
+  }
+
+  setLevel(): void {
     console.log('level', this.level);
     this.isMinLevel = this.level === 0;
-    this.isMaxLevel = this.level === (this.frases.length - 1)
+    this.isMaxLevel = this.level === (this.frases.length - 1);
     this.frase = this.frases[this.level];
     this.letters = [];
-    this.words = []
+    this.words = [];
     this.keys = [];
     let word = [];
     [...this.frase].forEach(l => {
-      const letra = new LetterModel(l)
+      const letra = new LetterModel(l);
       word.push(letra);
-      this.letters.push(letra)
+      this.letters.push(letra);
       if (letra.letra === ' ') {
         this.words.push(word);
         word = [];
@@ -88,6 +78,6 @@ export class HangmanComponent implements OnInit {
     });
     this.words.push(word);
 
-    [..."ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"].forEach(k => this.keys.push(this.getKey(k)))
+    [...'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'].forEach(k => this.keys.push(this.getKey(k)));
   }
 }
