3584cf53f7c7f10da1c70e0dc2e5ae2cf351235d
Daniel <drm.electronic@gmail.com>
2023-05-15 23:06:19 -0500

    minecraft

--- src/app/hangman/hangman.component.ts
+++ src/app/hangman/hangman.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FRASES} from '../frases/iglesia';
+import {FRASES} from '../frases/minecraft';
 import {KeyModel} from '../models/keyModel';
 import {LetterModel} from '../models/letterModel';
 
